3239277590dfb55b35b792eafb6ec95c139b5139
Lena Hou <lenahou2013@gmail.com>
2022-01-05 15:16:27 -0500

    updated three examples

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,8 +1,26 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect } from 'react';
 
 function Counter() {
 
     const [count, setCount] = useState(0)
+
+    // in order to make use of componentDidMount, we need to pass in an empty array as a second argument
+    useEffect(() => {
+        setCount(prevCount => prevCount + 1)
+        console.log("yo!")
+
+        // componentWillUnmount
+        // we call this cleanup function
+        return () => {
+           console.log("component unmounted")
+        }
+    }, [])
+
+    // componentDidUpdate
+    // pass a dependency or multiple dependencies as a second argument. The function will look into this argument and decide if it needs update
+    useEffect(() => {
+        console.log("count updated")
+    }, [count])
 
     function handleClick(){ 
         setCount(prevCount => prevCount + 1)
